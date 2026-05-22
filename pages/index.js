85b192b5ca6e2f2f1a12d84ba80bdb68916f227f
2786007 <105772197+2786007@users.noreply.github.com>
2026-05-22 08:40:48 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,50 @@
 export default function Home() {
-  return (
-    <div style={{
-      background:"#000",
-      color:"#fff",
-      minHeight:"100vh",
-      display:"flex",
-      justifyContent:"center",
-      alignItems:"center",
-      flexDirection:"column",
-      fontFamily:"Arial",
-      padding:"20px"
-    }}>
-      <img src="/logo.png" width="140" />
-      
-      <h1 style={{marginTop:"20px"}}>
-        ShreeNayraGroup
-      </h1>
+return (
+<div style={{
+background:"#000",
+color:"#fff",
+minHeight:"100vh",
+display:"flex",
+justifyContent:"center",
+alignItems:"center",
+flexDirection:"column",
+fontFamily:"Arial",
+padding:"20px"
+}}>
+<img src="/logo.png" width="140" />
 
-      <p>
-        Arun Kaul Official Business Platform
-      </p>
+<h1 style={{marginTop:"20px"}}>  
+    ShreeNayraGroup  
+  </h1>  
 
-      <img 
-        src="/arun.png"
-        width="220"
-        style={{
-          borderRadius:"20px",
-          marginTop:"20px"
-        }}
-      />
+  <p>  
+    Arun Kaul Official Business Platform  
+  </p>  
 
-      <a
-        href="https://wa.me/918826980675"
-        style={{
-          marginTop:"30px",
-          background:"#22c55e",
-          color:"#fff",
-          padding:"12px 20px",
-          borderRadius:"10px",
-          textDecoration:"none",
-          fontWeight:"bold"
-        }}
-      >
-        Contact on WhatsApp
-      </a>
-    </div>
-  )
+  <img   
+    src="/arun.png"  
+    width="220"  
+    style={{  
+      borderRadius:"20px",  
+      marginTop:"20px"  
+    }}  
+  />  
+
+  <a  
+    href="https://wa.me/918826980675"  
+    style={{  
+      marginTop:"30px",  
+      background:"#22c55e",  
+      color:"#fff",  
+      padding:"12px 20px",  
+      borderRadius:"10px",  
+      textDecoration:"none",  
+      fontWeight:"bold"  
+    }}  
+  >  
+    Contact on WhatsApp  
+  </a>  
+</div>
+
+)
 }
