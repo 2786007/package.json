502915ef1055c99edd7d2b768d2b90d7879184a7
2786007 <105772197+2786007@users.noreply.github.com>
2026-05-22 09:17:41 +0530

    Create app/page.js

---
+++ app/page.js
@@ -0,0 +1,36 @@
+export default function Home() {
+  return (
+    <main className="main">
+
+      <div className="hero">
+
+        <img
+          src="/logo.png"
+          className="logo"
+        />
+
+        <h1>SHREENAYRAGROUP</h1>
+
+        <p>
+          Hair Transplant • Finance • Insurance • Digital Marketing
+        </p>
+
+        <img
+          src="/arun.png"
+          className="profile"
+        />
+
+        <h2>Arun Kaul</h2>
+
+        <a
+          href="https://wa.me/918826980675"
+          className="btn"
+        >
+          Contact on WhatsApp
+        </a>
+
+      </div>
+
+    </main>
+  );
+}
